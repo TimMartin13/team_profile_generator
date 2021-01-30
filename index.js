b6035c43e404a15bd869a50b438681decd7daebc
Mandelbrot <timmartin13@gmail.com>
2021-01-30 10:15:08 -0600

    Working fully.

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // Page for the HTML render template
-const team = require('./src/page-template');
-
+const render = require('./src/page-template');
+// console.log(team);
 // Our main classes
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+const employees = [];
 // Main options
 const options = [
     {
         type: 'list',
         message: 'Would you like to?',
-        name: 'title',
+        name: 'role',
         choices: [
             'Add an Engineer',
             'Add an Intern',
@@ -28,65 +29,109 @@ function employeeQuestions(title) {
         {
             type: 'input',
             message: `What is the ${ title }'s name?`,
-            name: 'managerName',
+            name: 'employeeName',
         },
         {
             type: 'number',
             message: "Employee number?",
-            name: 'managerId',
+            name: 'employeeId',
         },
         {
             type: 'input',
             message: "Email address?",
-            name: 'managerEmail',
+            name: 'employeeEmail',
         }
     ]
     return questions;
 }
 
-// Question for Manager
-const questionManager = [
-    {
-        type: 'number',
-        message: "Office number?",
-        name: 'managerOffice',
+function getFourthQuestion(title) {
+    let question = [];
+    // Question for Manager
+    if (title === 'Manager') {
+        question = [
+            {
+                type: 'number',
+                message: "Office number?",
+                name: 'fourthAnswer',
+            }
+        ]
     }
-]
-
-// Question for Engineer
-const questionEngineer = [
-    {
-        type: 'input',
-        message: "Github username?",
-        name: 'engineerGithub',
+    
+    // Question for Engineer
+    if (title === 'Engineer') {
+        question = [
+            {
+                type: 'input',
+                message: "Github username?",
+                name: 'fourthAnswer',
+            }
+        ]
     }
-]
-
-// Question for Intern
-const questionIntern = [
-    {
-        type: 'input',
-        message: "School name?",
-        name: 'internSchool',
+    
+    // Question for Intern
+    if (title === 'Intern') {
+        question = [
+            {
+                type: 'input',
+                message: "School name?",
+                name: 'fourthAnswer',
+            }
+        ]
     }
-]
-
-function init() {
-    // Set title to manager
-    let title = 'Manager';
-
-    inquirer
-        .prompt(employeeQuestions(title))
-        .then((response) => console.log(response));
+    return question;
 }
 
-init();
-// Start application (init)
+function getTeamMember(title) {
+        
+    // Ask Everyone questions
+    inquirer
+        .prompt(employeeQuestions(title))
+        .then(({ employeeName, employeeId, employeeEmail }) => {
+            // Ask question based on title
+            inquirer
+                .prompt(getFourthQuestion(title))
+                .then(({ fourthAnswer }) => {
+                    switch(title) {
+                        case 'Manager': 
+                            employees.push(new Manager(employeeName, employeeId, employeeEmail, fourthAnswer));
+                            break;
+                        case 'Engineer': 
+                            employees.push(new Engineer(employeeName, employeeId, employeeEmail, fourthAnswer));
+                            break;
+                        case 'Intern': 
+                            employees.push(new Intern(employeeName, employeeId, employeeEmail, fourthAnswer));
+                            break;
+                    }
+                    inquirer
+                        .prompt(options)
+                        .then(({ role }) => {
+                            if (role.includes('Engineer'))      getTeamMember('Engineer');
+                            else if (role.includes('Intern'))   getTeamMember('Intern');
+                            else renderTeam();
+                        })
+                    // console.log(employees);
+                });
+        });
+}
+
+function renderTeam() {
+    console.log(employees);
+    // team.push(employees);
+    fs.writeFileSync('index.html', render(employees), (err) =>
+      err ? console.error(err) : console.log('Success!')
+    );
+}
+// Start application
+getTeamMember("Manager");
+
 // Set title to manager
 // Ask Everyone questions
-// Ask Manager question
-// Create new Manager and save
+// Ask question based on title
+// Create new (Title) and push to employee array
 // Prompt for Engineer, Intern or Finish
+    // set Title based on response, got to everyone questions
+
 // If Engineer
     // Change title to Engineer
     // Ask Everyone questions
@@ -100,4 +145,9 @@ init();
 
 
  // -------------------------------------------- Teacher pseudo code ----------------------------------//
-    
+
+// questions for the employee
+    // .then >> create Employee object
+        // type of employee
+            // if engineer >> create Engineer object
+            // else intern >> create Intern object
